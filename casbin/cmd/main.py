132b2c836ba5c80f19c7224d04fc7cb26b83eea6
Artyom <lauferartyom@vk.com>
2024-11-09 14:41:48 +0700

    added simple casbin

---
+++ casbin/cmd/main.py
@@ -0,0 +1,16 @@
+import casbin
+import datetime
+
+e = casbin.Enforcer("../configs/model.conf", "../configs/policy.csv")
+
+sub = "alice"     
+obj = "data"     
+act = "read"     
+date_of_reg = datetime.datetime.now().isoformat()
+
+# print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+if e.enforce(sub, obj, act, date_of_reg):
+    print("Access granted")
+else:
+    print("Access denied")
